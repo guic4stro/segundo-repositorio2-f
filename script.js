f6f21cd7fd9824b90365c62fa6594f582fa73716
guic4stro <guilherme.castro05@escola.pr.gov.br>
2025-06-26 22:08:53 -0300

    aula 2-1

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const botaoMostraPalavras = document.querySelector('#botao-palavrachave');
+}const botaoMostraPalavras = document.querySelector('#botao-palavrachave');
 
 botaoMostraPalavras.addEventListener('click', mostraPalavraChave);
 
@@ -6,7 +6,28 @@ function mostraPalavraChave() {
     //alert("Fui clicado!");
     const campoResultado = document.querySelector('#resultado-palavrachave');
     const texto = document.querySelector('#entrada-de-texto').value;
-    const palavras = texto.split("")
+    const palavras = processaTexto(texto)
 
     campoResultado.textContent = palavras.join(",");
+}
+function processaTexto(texto) {
+    //let palavras = texto.split(/\S+/);  "retirar espaço"
+    //let palavras = texto.split(/[^a-zA-]+/);  "letras minuscula e maiusculas"
+    //let palavras = texto.split(/\P{L}+/u/); /*\P negação; {L} conjunto de letras; + uma ou mais ocorrências; u  Unicode*/
+
+   let frequencias = [];
+
+   for (let i in palavras)
+      frequencias [i] = 0;
+    for ( led j in palavras){
+        if(palavras[i] == palavras[j]){
+            frequencias[i]++;
+        }
+    }
+
+}
+   
+   console.log(frequencias);
+   
+   return palavras;
 }
