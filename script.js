0c4224ad287bc1f8baf1eb3f30e4e184158584b5
guic4stro <guilherme.castro05@escola.pr.gov.br>
2025-06-26 21:27:47 -0300

    Aula2- 26/06

--- script.js
+++ script.js
@@ -3,5 +3,9 @@ const botaoMostraPalavras = document.querySelector('#botao-palavrachave');
 botaoMostraPalavras.addEventListener('click', mostraPalavraChave);
 
 function mostraPalavraChave() {
-    alert("Fui clicado!");
+    //alert("Fui clicado!");
+    const campoResultado = document.querySelector('#resultado-palavrachave');
+    const texto = document.querySelector('#entrada-de-texto').value;
+    
+    campoResultado.textContent = texto;
 }
