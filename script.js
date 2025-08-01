67f6b4f6512fbf739b35c388bdbca89729dca1b5
guic4stro <guilherme.castro05@escola.pr.gov.br>
2025-07-31 21:19:46 -0300

    Aula 8 31/07

--- script.js
+++ script.js
@@ -1,20 +1,28 @@
-import  botaoMostraPalavras = document.querySelector('#botao-palavrachave');
+import {PALAVRAS_RUINS} from "./palavrasRuins.js";
 
-botaoMostraPalavras.addEventListener('click', mostraPalavraChave);
+const botaoMostraPalavras = document.querySelector('#botao-palavrachave');
+
+botaoMostraPalavras.addEventListener('click', mostraPalavrasChave);
 
 function mostraPalavraChave() {
     //alert("Fui clicado!");
     const texto = document.querySelector('#entrada-de-texto').value;
     const campoResultado = document.querySelector('#resultado-palavrachave');
-    const palavras = processaTexto(texto);
+    const palavrasChave = processaTexto(texto);
 
-    campoResultado.textContent = palavras.join(", ");
+    campoResultado.textContent = palavrasChave.join(", ");
 }
 
 function processaTexto(texto) {
     //let palavras = texto.split(/\s+/);  "retirar espaços"
     //let palavras = texto.split(/[^a-zA-Z]+/); "letras minusculas e maiusculas"
-    let palavras = texto.split("/\P{L}+/u/");  /* \P negação;  {L} conjunto de letras; + uma ou mais ocorrências; u Unicode*/
+    let palavras = texto.split(/\P{L}+/u/);  /* \P negação;  {L} conjunto de letras; + uma ou mais ocorrências; u Unicode*/
+
+    for (let i in palavras) {
+        palavras[i] = palavras[i].toLowerCase();
+    }
+
+    palavras = tiraPalavrasRuins(palavras);
 
     const frequencias = contaFrequencias(palavras);
 
